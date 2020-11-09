0d4b821756c867024cdb96ec61167d8edcbf881f
pragy30 <pragyachaube@gmail.com>
2020-11-10 00:00:25 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -4,6 +4,6 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon', hour=22)
 def scheduled_job():
-    print('This job is run every weekday at 10pm.')
+    print('This job is run every Monday at 10pm.')
 
 sched.start()
