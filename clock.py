89b222393e79f0729347b3c3c75727fa6955a402
pragy30 <pragyachaube@gmail.com>
2020-11-09 23:50:52 +0530

    Create clock.py
    Every Monday at 10 pm

---
+++ clock.py
@@ -0,0 +1,9 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', day_of_week='mon', hour=22)
+def scheduled_job():
+    print('This job is run every weekday at 10pm.')
+
+sched.start()
